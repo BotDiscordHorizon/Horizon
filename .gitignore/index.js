723bfdb2f7efc0469d64fa8c42dd4042a4e8f4a2
BotDiscordHorizon <47712922+BotDiscordHorizon@users.noreply.github.com>
2019-02-17 13:45:28 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,11 +1,15 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+const fs = require("fs");
+const scores = require("./scores.json");
+typeof scores; 
 
 var prefix = ("~")
 
 bot.on('ready', function() {
-    bot.user.setGame("Æna  ~help");
+    bot.user.setGame("Æna ★ : ~help");
     console.log("Connectedç");
+    bot.user.setNote("Found a New Horizon")
 });
 
 bot.login(process.env.Token1);
@@ -254,17 +258,19 @@ bot.on('message', message => { //OTHERS
   if(message.content === "~ping"){
     message.channel.sendMessage('Temps de latence avec le serveur: `' + `${message.createdTimestamp - Date.now()}` + 'ms`');
   };
+
+  if(message.content === "~avatar"){
+       message.reply("Merci de mentionner un utilisateur valide avec la commande ``~avatar <Utilisateur>``")
+    }else{
        if(message.content.startsWith('~avatar')) {
         let user = message.mentions.users.first() || message.author;
            let embed = new Discord.RichEmbed()
-           .setDescription(`**Avatar de ${user}**`)
+           .setTitle(`Avatar de <@${user}>`)
          .setImage(user.avatarURL)
          .setColor('RANDOM')
            message.channel.send(embed);
-       };
-
-        })
-
+         }};
+});
 
 bot.on('message', message => {//RANDOM
 var randnum = 0
@@ -277,121 +283,159 @@ var randnum = 0
 
 if (message.content === "~rndm"){
   randomc()
-  if (randnum ==1){
+  switch(randnum){
+    case 1:
     var embed = new Discord.RichEmbed()
     .setDescription("1")
     .setImage("https://media.giphy.com/media/l44QvKoQuUD3xPZKg/giphy.gif")
    .setColor("RANDOM")
 message.channel.sendEmbed(embed)
-      console.log(randnum);
-  }
-
-  if (randnum ==2){
+    break;
+    case 2:
     var embed = new Discord.RichEmbed()
     .setDescription("2")
     .setImage("https://media.giphy.com/media/nwCz29GZlx0HfZZIwF/giphy.gif")
    .setColor("RANDOM")
 message.channel.sendEmbed(embed)
-      console.log(randnum);
-  }
-
-  if (randnum ==3){
+    break;
+    case 3:
     var embed = new Discord.RichEmbed()
     .setDescription("3")
     .setImage("https://media.giphy.com/media/zcwCC4GqKznj2/giphy.gif")
    .setColor("RANDOM")
 message.channel.sendEmbed(embed)
-      console.log(randnum);
+    break;
   }
 };
 
 var randhug = 0
 
   function random(min, max){
-    min = Math.ceil(1);
+    min = Math.ceil(0);
     max = Math.floor(4);
     randhug = Math.floor(Math.random() * (max - min +1)+ min);
 }
 
 if (message.content === "Hugs"){
   random()
-  
-  if (randhug ==1){
+
+  switch(randhug){
+    case 1:
     var embed = new Discord.RichEmbed()
     .setDescription("***Tiens un câlin ! :D***")
    .setColor("#8800fc")
 message.channel.sendEmbed(embed)
-      console.log(randhug);
-  }
-
-  if (randhug ==2){
-    var embed = new Discord.RichEmbed()
+break;
+case 2:
+var embed = new Discord.RichEmbed()
     .setDescription("***Non ?! T'as cru que j'étais ton pote ?*** ")
    .setColor("#000000")
 message.channel.sendEmbed(embed)
-      console.log(randhug);
-  }
-
-  if (randhug ==3){
-    var embed = new Discord.RichEmbed()
+break;
+case 3:
+var embed = new Discord.RichEmbed()
     .setDescription("***Petit câlin - qui fait du bien.~***")
    .setColor("#C71585")
 message.channel.sendEmbed(embed)
-      console.log(randhug);
-  }
-
-  if (randhug ==4){
-    var embed = new Discord.RichEmbed()
+break;
+case 4:
+var embed = new Discord.RichEmbed()
     .setDescription("***Full of hugs for you!*** ")
    .setColor("#FFB6C1")
 message.channel.sendEmbed(embed)
-      console.log(randhug);
+break;
   }
 }
+      var randlovecalc = 0
 
-if(message.content === "~clear"){
-  message.reply("Merci de préciser le nombre de message à supprimer avec la commande ``~clear <nombre>")
-}else{
-  if(message.content.startsWith("~clear")){
-    if(!message.member.hasPermission("KICK_MEMBERS")){
-      message.reply("Vous n'avez pas la permission")
+      function random(min, max){
+        min = Math.ceil(0);
+        max = Math.floor(100);
+        randlovecalc = Math.floor(Math.random() * (max - min +1)+ min);
+    }
+  
+    if(message.content === "~lovecalc"){
+      message.reply("Merci d'utiliser la commande correctement avec ``~lovecalc <utilisateur>``")
     }else{
-      var limita = message.content.slice(7)
-      var limitao = parseInt(limita)
-      message.channel.fetchMessages()
-          .then(messages => {
-            message.channel.bulkDelete(limitao + 1);
-            messagesDeleted = messages.array().length
-            var embedw = new Discord.RichEmbed()
-         .setColor('#FF4500')
-         .setDescription(`Messages supprimés avec succés. \n Total  des messages supprimés : ` + limitao)
-         message.channel.sendEmbed(embedw)
-         setTimeout(() => {message.channel.bulkDelete(1);}, 60 * 50)
-          })
-      }}};
+      if(message.content.startsWith("~lovecalc")){
+        let auteur = message.author
+        let lovecal = String(randlovecalc)
+        let memberd = message.mentions.members.first();
+        var colorlove = "RANDOM"
+        var textlove = "abc"
+        let args = message.content.split(' ')
+        if(randlovecalc >= 50){
+          colorlove = "#00FF00"
+          textlove = "A croire qu'ils sont fait l'un pour l'autre !"
+        }
+        if((randlovecalc <= 50) && (randlovecalc >= 25)){
+          colorlove = "#FFA500"
+          textlove ="Qui ne tente rien n'a rien."
+        }
+        if((randlovecalc <= 25) && (randlovecalc >= 0)){
+          colorlove = "#FF0000"
+          textlove = "Rien à faire.. Clairement."
+        }
+        if(!args[1].includes("@")){
+          message.reply("Merci d'utiliser la commande correctement avec ``~lovecalc <utilisateur>``")
+        }else{
+      var embedlove = new Discord.RichEmbed()
+      .setColor(colorlove)
+      .setDescription(`Le love score de ${auteur} avec ${memberd} est de ${lovecal} :heartbeat: !!. \n ${textlove}`)
+      message.channel.sendEmbed(embedlove)
+        }
+      }
+    }
 });
 
 bot.on('message', msg => {//MODOCMD
-  if (msg.content === "~mute"){
-    msg.reply("Vous devez mentionner un utilisateur à mute avec ``~mute <nom>``");
+
+  if(msg.content === "~clear"){
+    msg.reply("Merci de préciser le nombre de message à supprimer avec la commande ``~clear <nombre>")
   }else{
-    if(msg.content.startsWith("~mute")){
+    if(msg.content.startsWith("~clear")){
+      if(!msg.member.hasPermission("KICK_MEMBERS")){
+        msg.reply("Vous n'avez pas la permission")
+      }else{
+        var limita = msg.content.slice(7)
+        var limitao = parseInt(limita)
+        msg.channel.fetchMessages()
+            .then(msg => {
+              msg.channel.bulkDelete(limitao + 1);
+              messagesDeleted = msg.array().length
+              var embedw = new Discord.RichEmbed()
+           .setColor('#FF4500')
+           .setDescription(`Messages supprimés avec succés. \n Total  des messages supprimés : ` + limitao)
+           msg.channel.sendEmbed(embedw)
+           setTimeout(() => {msg.channel.bulkDelete(1);}, 60 * 50)
+            })
+        }}};
+
+  if (msg.content === "~mute"){
+    msg.reply("Vous devez mentionner un utilisateur à mute avec ``!!mute <heures> <mention>``");
+ }else{
+     if(msg.content.startsWith("!mute")){
        if(!msg.member.hasPermission("KICK_MEMBERS")){
-       msg.reply("Vous n'avez pas la permission de mute !")
-     }else{
-      let mute_role = msg.guild.roles.find("name", "Muted");
-      let memberd = msg.mentions.members.first();
-      memberd.addRole(mute_role)
-      setTimeout(() => {memberd.removeRole(mute_role);}, 60 * 20000)
-      var embedsys = new Discord.RichEmbed()
-      .setColor('#FFFF63')
-      .setTitle(`Mute de 20 minutes effectué avec succés !`)
-      .setDescription(`${memberd} s'est fait mute par ${msg.author} le temps de 20 minutes..`)
-      .setImage("https://media.giphy.com/media/zcwCC4GqKznj2/giphy.gif")
-      .setFooter("Æna ★")
-      .setAuthor("New Horizon")
-      msg.channel.sendEmbed(embedsys)
-}}};
+         msg.reply("Vous n'avez pas la permission de mute !")
+       }else{
+         let args = msg.content.split(' ')
+         if(Number.isInteger(parseInt(args[1]))){
+           let arg = parseInt(args[1])
+          let mute_role = msg.guild.roles.find("name", "Muted");
+           let memberd = msg.mentions.members.first();
+           memberd.addRole(mute_role)
+           setTimeout(() => {memberd.removeRole(mute_role);}, 3600000 * arg)
+           var embedsys = new Discord.RichEmbed()
+           .setColor('#FFFF63')
+           .setTitle(`Mute de ${args[1]} heure(s) effectué avec succés !`)
+           .setDescription(`${memberd} s'est fait mute par ${msg.author} le temps de ${args[1]} heure(s)..`)
+           .setImage("https://media.giphy.com/media/zcwCC4GqKznj2/giphy.gif")
+           .setFooter("Aena ★")
+           .setAuthor("New Horizon")
+           msg.channel.sendEmbed(embedsys)
+        }else{
+       msg.reply("Merci de mentionner une durée valide avec la commande ``!!mute <heures> <@utilisateur>``.")
+     }}}};
 
   if (msg.content === "~ban"){
       msg.reply("Vous devez mentionner un utilisateur à ban avec ``~ban <nom>``");
@@ -572,9 +616,8 @@ bot.on('message', msg => {
         .setImage('https://cdn.discordapp.com/attachments/474923379590168596/516294112375209999/Multi_Color_Bar.gif')
         .setColor("RANDOM")
         .setFooter("Æna ★")
-        .setAuthor("New Horizon")
            msg.author.sendEmbed(embed1).then(msg =>{
-             msg.channel.send("Mp envoyé !!")
+             msg.reply("Mp envoyé !!")
            })
 }
 
